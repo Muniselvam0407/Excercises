9465e27663edd5795c32591804d3b25e368eddbc
Poongkodiselvam <Poongkodiselvam@DESKTOP-0DR7BCA>
2023-03-04 14:22:50 +0530

    facebook login with screenshot

---
+++ SeleniumExcercise/src/selenium/fbLogin.java
@@ -0,0 +1,54 @@
+package selenium;
+
+import java.io.File;
+import java.io.IOException;
+import java.time.Duration;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class fbLogin {
+
+	static WebDriver driver;
+	
+	public static void main(String[] args) {
+
+		System.setProperty("webdriver.chrome.driver", 
+				"E:\\Testing\\New folder\\chromedriver_win32\\chromedriver.exe");
+		WebDriver driver= new ChromeDriver();
+		
+		driver.manage().window().maximize();	
+		
+		driver.get("https://www.facebook.com/");
+		driver.findElement(By.id("email")).sendKeys("munis239");
+		driver.findElement(By.id("pass")).sendKeys("1234");
+		WebElement button = driver.findElement(By.name("login"));
+		button.click();
+		
+		TakesScreenshot TS= (TakesScreenshot)driver;
+		
+		File screenshotAs = TS.getScreenshotAs(OutputType.FILE);
+	//	System.out.println(screenshotAs);
+		
+		File destination= new File("C:\\Users\\Poongkodiselvam\\Desktop\\Screenshot\\fblogin.png");
+		
+		try {
+			FileUtils.copyFile(screenshotAs, destination);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+		
+		
+		driver.quit();
+		
+	}
+	
+
+}
