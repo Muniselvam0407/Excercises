870498c62ac38c1da21c11d1d3ee4c9b5aef9d2f
Poongkodiselvam <Poongkodiselvam@DESKTOP-0DR7BCA>
2023-03-04 15:09:52 +0530

    facebook login with screenshot

---
+++ SeleniumExcercise/src/selenium/printScreen.java
@@ -0,0 +1,33 @@
+package selenium;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+
+public class printScreen {
+
+	static WebDriver driver;
+	
+	public static void screenshot() {
+
+		TakesScreenshot TS= (TakesScreenshot)driver;
+		
+		File screenshotAs = TS.getScreenshotAs(OutputType.FILE);
+		//System.out.println(screenshotAs);
+		
+		File destination= new File("C:\\Users\\Poongkodiselvam\\Desktop\\Screenshot\\fblogin.png");
+		
+		try {
+			FileUtils.copyFile(screenshotAs, destination);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+
+}
